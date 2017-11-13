44cc00fff6366a5da6b2f2a9a84b925638fc083e
jfqd <jfqd@blun.org>
2017-11-13 10:12:31 +0100

    replay stored form-data by a given token

--- lib/mailer_tags_extensions.rb
+++ lib/mailer_tags_extensions.rb
@@ -9,7 +9,31 @@ module MailerTagsExtensions
   }
   tag 'mailer:to_json' do |tag|
     mail = tag.locals.page.last_mail
-    mail.to_json
+    mail.data.to_json
+  end
+  
+  def prior_value(tag, tag_name=tag.attr['name'])
+    if mail = tag.locals.page.last_mail
+      h(mail.data[tag_name]) unless StringIO === mail.data[tag_name] or Tempfile === mail.data[tag_name]
+    else
+      prior_database_value(tag_name)
+    end
+  end
+  
+  private
+  
+  def prior_database_value(tag_name)
+    @json_hash ||= json_hash
+    @json_hash.present? ? @json_hash[tag_name] : nil
+  end
+  
+  def json_hash
+    begin
+      fd = FormData.find_by_token(@request.parameters[:token])
+      fd.present? ? JSON.parse(fd.json_field) : nil
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
   end
   
 end
