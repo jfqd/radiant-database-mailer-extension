a552385d1e4677a4dafef148fd29af985424f182
jfqd <jfqd@blun.org>
2018-12-20 12:02:53 +0100

    bump version to 1.1.4

--- lib/radiant-database_mailer-extension.rb
+++ lib/radiant-database_mailer-extension.rb
@@ -1,5 +1,5 @@
 module RadiantDatabaseMailerExtension
-  VERSION     = '1.1.3'
+  VERSION     = '1.1.4'
   SUMMARY     = %q{Database mailer for Radiant CMS}
   DESCRIPTION = %q{Save fields from mailer forms to the database.}
   URL         = "http://blog.aissac.ro/radiant/database-mailer-extension/"
